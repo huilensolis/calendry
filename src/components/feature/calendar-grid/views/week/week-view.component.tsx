22e66ad8827d86a54dfe0db0c8d566bbcc98e6a0
Huilensolis <xywvanz@gmail.com>
2024-04-16 23:27:14 -0300

    feat(app): 12 add week numbers and days names to calendar header

--- src/components/feature/calendar-grid/views/week/week-view.component.tsx
+++ src/components/feature/calendar-grid/views/week/week-view.component.tsx
@@ -1,8 +1,34 @@
+import { cn } from "@/lib/utils/cn";
 import { Column } from "../../column";
 import { TimeAside } from "../../time-aside";
 
 export function WeekView() {
-  const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const daysOfWeek = [" ", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  const thisWeek: { date: number; day: string }[] = [];
+
+  const today = new Date();
+
+  const startOfWeek = new Date().getDate() - new Date().getDay() + 1;
+
+  for (let i = 0; i < 7; i++) {
+    const current = new Date(startOfWeek);
+    current.setDate(startOfWeek + i);
+
+    let dayOfWeek: number;
+
+    if (current.getDay() === 0) {
+      dayOfWeek = 7;
+    } else {
+      dayOfWeek = current.getDay();
+    }
+
+    thisWeek.push({
+      date: current.getDate(),
+      day: daysOfWeek[dayOfWeek],
+    });
+  }
+
   return (
     <div className="flex flex-col w-full">
       <div className="w-full flex bg-neutral-100 border-b border-neutral-200 sticky top-[73px]">
@@ -10,10 +36,18 @@ export function WeekView() {
           GTM-3
         </span>
         <ul className={`w-full flex-1 grid grid-cols-7`}>
-          {daysOfWeek.map((day, i) => (
+          {thisWeek.map(({ day, date }, i) => (
             <li key={i} className="flex flex-col h-full">
-              <header className="py-2 text-center border-r border-neutral-200">
-                {day}
+              <header
+                className={cn(
+                  "py-2 text-center border-r border-neutral-200",
+                  `${
+                    date === new Date().getDate() &&
+                    "bg-primary text-primary-foreground"
+                  }`,
+                )}
+              >
+                {day} {date}
               </header>
             </li>
           ))}
@@ -24,9 +58,8 @@ export function WeekView() {
           <TimeAside />
         </div>
         <ul className={`w-full flex-1 grid grid-cols-7 overflow-auto`}>
-          {daysOfWeek.map((day, i) => (
+          {thisWeek.map(({ day, date }, i) => (
             <li key={i} className="w-full">
-              {" "}
               <Column />
             </li>
           ))}
